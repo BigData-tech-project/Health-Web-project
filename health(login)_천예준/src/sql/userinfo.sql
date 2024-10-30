6ee3dbe54879fe6007f7a7c9bf31432e08353c6c
BigData-tech-project <jsuhyeon363@gmail.com>
2024-10-30 09:46:05 +0900

    Delete health(login)_천예준 directory

--- health(login)_천예준/src/sql/userinfo.sql
+++
@@ -1,8 +0,0 @@
-SELECT * FROM userinfo;
-
-INSERT INTO userinfo (id, pw, name, gender, age) VALUES 
-	('1', '1', 'as', 'Male', 20),
-	('2', '2', 'ad', 'Female', 21),
-	('3', '3', 'af', 'Male', 22),
-	('4', '4', 'ag', 'Female', 23),
-	('5', '5', 'ah', 'Male', 24);
