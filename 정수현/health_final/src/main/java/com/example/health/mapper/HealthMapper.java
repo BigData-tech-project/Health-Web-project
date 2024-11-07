fe6f7800c6c59482004400a7bd8c665595501ea9
BigData-tech-project <jsuhyeon363@gmail.com>
2024-11-07 17:00:02 +0900

    Add files via upload

---
+++ 정수현/health_final/src/main/java/com/example/health/mapper/HealthMapper.java
@@ -0,0 +1,18 @@
+package com.example.health.mapper;
+
+import java.util.List;
+
+import com.example.health.model.HealthVo;
+import com.example.health.model.UserVo;
+
+public interface HealthMapper {
+	
+	HealthVo getHealthById(String id);
+	
+	void insertHealth(HealthVo healthVo);
+	
+	List<HealthVo> findAllByUserId(String userId);
+
+	HealthVo getUserInfoById(String id);
+	
+}
